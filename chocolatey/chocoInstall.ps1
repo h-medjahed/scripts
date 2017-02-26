bcb130c2ccd01a198d9900cd003064953fc69384
h-medjahed <h.medjahed@gmail.com>
2017-02-26 20:32:10 +0100

    Update chocoInstall.ps1
    update

--- chocolatey/chocoInstall.ps1
+++ chocolatey/chocoInstall.ps1
@@ -1,7 +1,7 @@
-#This is a  PowerShell script , to install chocolatey, The package manager for Windows
-#Chocolatey builds on technologies you know - unattended installation and PowerShell.
-#Chocolatey works with all existing software installation technologies like MSI, NSIS,
-#InnoSetup, etc, but also works with runtime binaries and zip archives.
+# This is a  PowerShell script , to install chocolatey, The package manager for Windows
+# Chocolatey builds on technologies you know - unattended installation and PowerShell.
+# Chocolatey works with all existing software installation technologies like MSI, NSIS,
+# InnoSetup, etc, but also works with runtime binaries and zip archives.
 # Written by Hamid MEDJAHED
 
 Set-ExecutionPolicy Unrestricted
