c11caf815e0b9fadc99c63fd4b1be96bf8302754
h-medjahed <h.medjahed@gmail.com>
2017-03-27 15:29:42 +0200

    Update tomcat7.ps1

--- chocolatey/tomcat7.ps1
+++ chocolatey/tomcat7.ps1
@@ -1,18 +1,15 @@
-cls
+# This is a  PowerShell script , to install chocolatey, The package manager for Windows
+# Chocolatey builds on technologies you know - unattended installation and PowerShell.
+# Chocolatey works with all existing software installation technologies like MSI, NSIS,
+# InnoSetup, etc, but also works with runtime binaries and zip archives.
+# Example of installinf tomcat7 server with chocolatey
+# Written by Hamid MEDJAHED
+
 Set-ExecutionPolicy Unrestricted
 
-[string]$Depot = "https://raw.githubusercontent.com/jewelro/PSscripts/master/"
-[string]$DepotChoco = "https://chocolatey.org/install.ps1"
-[string]$CurrentDir = $(get-location).Path
-[string]$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-
 # Log file
-[string]$fileName = "scriptInstall"
+[string]$fileName = "tomcatinstalllog"
 [string]$logfile = $CurrentDir + '\\' + $fileName + '.log'
-
-# Transcription file
-[string]$transcriptPath = $CurrentDir + '\\' + $fileName + 'Transcript.txt'
-Start-Transcript -Path $transcriptPath -Force -Append -NoClobber
 
 # WritetoLog
 function Write-toLog {
@@ -26,46 +23,10 @@ function Write-toLog {
 	Write-Output $logMessage2
 }
 
-# Install function
-function Install-GitFile {
-	[cmdletbinding()]
-	param (
-		[Parameter(Mandatory=$true)][string]$installFile,
-		[Parameter(Mandatory=$false)][string[]]$argList
-	) 
-	$logmess = "install: $installFile : args : $argList"
-	Write-toLog -logMessage $logmess
-	if ($argList){
-		icm $executioncontext.InvokeCommand.NewScriptBlock((New-Object Net.WebClient).DownloadString($installFile)) -ArgumentList $argList
-	}
-	else {
-		icm $executioncontext.InvokeCommand.NewScriptBlock((New-Object Net.WebClient).DownloadString($installFile)) 
-	}
-	#wait 2 seconds 
-	Start-Sleep -m 2000
-}
+#Install chocolatey
+Write-toLog -logMessage "install chocolatey"
+$chocourl="https://chocolatey.org/install.ps1"
+icm $executioncontext.InvokeCommand.NewScriptBlock((New-Object Net.WebClient).DownloadString($chocourl))
 
-Write-toLog -logMessage "start execution $fileName"
-
-try {
-	# install chocolatey
-	$myfile = $DepotChoco
-	#$myfile = $Depot + "chocoInstall.ps1"
-	Install-GitFile -installFile $myfile
-
-	# install Tomcat 7
-	Write-toLog -logMessage "choco install tomcat -version 7.0.59"
-	Invoke-Expression("choco install tomcat -version 7.0.59 -y")
-
-	# Exit
-	Write-toLog -logMessage "end execution $fileName"
-	stop-transcript
-}
-catch {
-  Write-Output "EXCEPTION : "
-  $ErrorMessage = $_.Exception.Message
-  $FailedItem = $_.Exception.ItemName
-  Write-toLog -logMessage $ErrorMessage
-  stop-transcript
-  Throw $_.Exception
-}
+Write-toLog -logMessage "choco install tomcat -version 7.0.59"
+Invoke-Expression("choco install tomcat -version 7.0.59 -y")
